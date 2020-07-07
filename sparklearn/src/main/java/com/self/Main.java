72bb2976433e49c02738b501d13ba7009c2b1be4
anan-2019 <2474296512@qq.com>
2020-07-07 14:34:19 +0800

    添加文件

---
+++ sparklearn/src/main/java/com/self/Main.java
@@ -0,0 +1,15 @@
+package com.self;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName Main
+ * @Author 51205
+ * @Date 2020/7/7 14:26
+ * @Version 1.0
+ * @Description Class Function Description Here
+ */
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+}
