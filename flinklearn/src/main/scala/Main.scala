ee362a85c981c5f86560eea1f204ba17024c053e
anan-2019 <2474296512@qq.com>
2020-07-07 17:01:59 +0800

    测试修改新的项目

---
+++ flinklearn/src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Main
+ * @Author 51205
+ * @Date 2020/7/7 17:01
+ * @Version 1.0
+ * @Description Class Function Description Here
+ */
+object Main {
+    def main(args: Array[String]): Unit = {
+        print("测试")
+    }
+}
