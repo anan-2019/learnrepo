f90fb013957c0d656ba7362f6a1ecd328ec76844
anan-2019 <2474296512@qq.com>
2020-07-07 15:17:33 +0800

    测试修改

---
+++ scalalearn/src/main/scala/com/self/Main.scala
@@ -0,0 +1,14 @@
+package com.self
+
+/**
+ * @ClassName Main
+ * @Author 51205
+ * @Date 2020/7/7 15:11
+ * @Version 1.0
+ * @Description Class Function Description Here
+ */
+object Main {
+    def main(args: Array[String]): Unit = {
+        print("测试")
+    }
+}
