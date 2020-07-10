44cd5e53fd75c9c2a57f6afdad0874962267092b
anan-2019 <2474296512@qq.com>
2020-07-10 08:50:53 +0800

    添加dockerfile创建docker镜像的方式

---
+++ javalearn/dockerbuild/src/main/java/com/self/javalearn/dockerbuild/Main.java
@@ -0,0 +1,19 @@
+package com.self.javalearn.dockerbuild;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName Main
+ * @Author 51205
+ * @Date 2020/7/9 17:12
+ * @Version 1.0
+ * @Description Class Function Description Here
+ */
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("success to start！！");
+    }
+}
